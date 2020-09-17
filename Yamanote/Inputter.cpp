de51f854412d1cf234e911dabd0f9a55f805cc73
seiryu725 <siryu725@gmail.com>
2020-09-17 11:23:37 +0900

    終了判定追加

--- Yamanote/Inputter.cpp
+++ Yamanote/Inputter.cpp
@@ -1,4 +1,5 @@
 ﻿#include "Inputter.h"
+#include <conio.h>
 
 /* 入力待ち関数 */
 void Inputter::InputStation()
@@ -8,4 +9,24 @@ void Inputter::InputStation()
 
     printf("終わりの駅名を入力してください。\n");
     scanf_s("%s", &m_end_station_name, 64);
+}
+
+/* 終了待ち関数 */
+bool Inputter::InputEnd()
+{
+    int end;  //終了キー番号保存
+
+    printf("終了する場合Escを、続ける場合他のキーを押してください\n");
+
+    end = _getch();  //入力
+
+    /* Escならばtrue */
+    if (end == ESC)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
--- Yamanote/Inputter.h
+++ Yamanote/Inputter.h
@@ -1,6 +1,9 @@
 #ifndef INPUTTER_H_
 #define INPUTTER_H_
 #include <string>
+#include <stdio.h>
+
+#define ESC 27  //Esc”Ô†
 
 class Inputter
 {
@@ -8,6 +11,8 @@ public:
 	Inputter() {}
 	~Inputter() {}
 	void InputStation();   //‰w–¼“ü—ÍŠÖ”
+	bool InputEnd();       //I—¹ƒL[“ü—ÍŠÖ”
+
 
 	const char* GetStartStationName() { return m_start_station_name; }   //Å‰‚Ì‰w–¼‚ğ•Ô‚·
 	const char* GetEndStationName() { return m_end_station_name; }       //ÅŒã‚Ì‰w–¼‚ğ•Ô‚·
